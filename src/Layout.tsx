0ee854135521bce375c849f87e935940fc46c6c0
tetsuyayamamoto0324 <manpuku769@gmail.com>
2025-05-18 00:48:58 +0900

    first commit

---
+++ src/Layout.tsx
@@ -0,0 +1,28 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import { SearchModal } from './components/SearchModal';
+import { useCurrentUserStore } from './modules/auth/current-user.state';
+
+const Layout = () => {
+  const { currentUser } = useCurrentUserStore();
+
+  if(currentUser == null) return <Navigate replace to="/signin" />
+
+  return (
+    <div className="h-full flex">
+      <SideBar onSearchButtonClicked={() => {}} />
+      <main className="flex-1 h-full overflow-y-auto">
+        <Outlet />
+        <SearchModal
+          isOpen={false}
+          notes={[]}
+          onItemSelect={() => {}}
+          onKeywordChanged={() => {}}
+          onClose={() => {}}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
