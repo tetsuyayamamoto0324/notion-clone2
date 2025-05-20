7f14a08329d0430497e6e09a0162db6fef2e625b
tetsuyayamamoto0324 <manpuku769@gmail.com>
2025-05-20 20:32:27 +0900

    途中2

--- src/modules/notes/note.state.ts
+++ src/modules/notes/note.state.ts
@@ -19,8 +19,11 @@ export const useNoteStore = () => {
     });
   };
 
+  const getOne = (id:number) => notes.find((note) => note.id == id);
+
   return {
     getAll: () => notes,
+    getOne,
     set,
   };
 };
